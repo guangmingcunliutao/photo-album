c84e79be104600673fc57ef99a6b630580a79f2a
liu.tao <1018694341@qq.com>
2025-11-17 16:05:49 +0800

    feat: 改hash路由

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,8 +1,8 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import generatedRoutes from 'virtual:generated-pages'
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHashHistory(import.meta.env.BASE_URL),
   routes: [
     { path: '/', redirect: '/album' },
     ...generatedRoutes,
